92cf7e604fa363a6a4bf908ae4b4ef61a3195441
jesusmarzor <jesusmarzor28@gmail.com>
2024-09-22 23:12:14 +0200

    Add: useDate

---
+++ app/hooks/useDate.ts
@@ -0,0 +1,25 @@
+import { minDaysNewLabel } from "@/app/utils/constants";
+
+export const useDate = () => {
+
+    const getDateFromString = (dateString: string): Date => new Date(Date.parse(String(dateString)))
+
+    const secondsBetweenDates = (startDate: Date, endDate: Date) => endDate.getTime() - startDate.getTime()
+
+    const getDateString = (dateString: string): string => {
+        const options: Intl.DateTimeFormatOptions = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' };
+        return getDateFromString(dateString).toLocaleDateString("es", options)
+    }
+
+    const isShowedNew = (dateString: string): boolean => {
+        const secondsBetween = secondsBetweenDates(getDateFromString(dateString), new Date())
+        const daysBetween = secondsBetween / (1000 * 3600 * 24)
+        return minDaysNewLabel > daysBetween
+    }
+
+    const isShowedPost = (dateString: string): boolean => getDateFromString(dateString).getTime() <= new Date().getTime()
+
+    return {getDateString, isShowedNew, isShowedPost}
+}
+
+export default useDate
