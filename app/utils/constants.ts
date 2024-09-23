6a20835c953dc57d83c056fe2f42c6c0dbc60f5e
jesusmarzor <jesusmarzor28@gmail.com>
2024-09-23 23:16:13 +0200

    Add: new constants

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -4,7 +4,9 @@ export const textLogo = {
     name: "Jesus",
     lastName: "marzor"
 }
+export const authorName = "Jesús Martín"
 
 export const minDaysNewLabel = 3;
+export const maxNumCardsInHomeSections = 3;
 
 export const posts: Array<Post> = []
