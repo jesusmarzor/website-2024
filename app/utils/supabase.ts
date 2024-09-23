3a2a9564b8c5b0aceebd32360c6eba23e25b958d
jesusmarzor <jesusmarzor28@gmail.com>
2024-09-23 23:17:43 +0200

    Add: supabase config

---
+++ app/utils/supabase.ts
@@ -0,0 +1,10 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl: string = "https://" + process.env.NEXT_PUBLIC_SUPABASE_URL ?? ""
+const supabaseKey: string = process.env.NEXT_PUBLIC_SUPABASE_SECRET_ROLE ?? ""
+
+export default createClient(supabaseUrl, supabaseKey, {
+    auth: {
+      persistSession: false,
+    }
+})
