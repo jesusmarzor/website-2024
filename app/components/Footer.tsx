650df50fa3241b876578bb333146cdb623be6c6d
jesusmarzor <jesusmarzor28@gmail.com>
2024-09-29 17:09:31 +0200

    Fix: footer info

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,12 +1,25 @@
-import Logo from "@/app/components/Logo"
+import Logo from "@/app/components/Logo";
 
 const Footer = () => {
-    return (
-        <footer className="w-full flex flex-col gap-4 justify-center items-center px-10 pb-10">
-            <Logo isFullName={false}/>
-            <p className="text-sm">Inspirado en el proyecto de <a className="text-main" href="https://moure.dev" target="_blank" rel="noreferrer">@mouredev</a></p>
-        </footer>
-    )
-}
+  return (
+    <footer className="w-full flex flex-col gap-4 justify-center items-center px-10 pb-10">
+      <Logo isFullName={false} />
+      <div className="text-center">
+        <p className="text-sm">Web desarrollada por jesusmarzor.</p>
+        <p className="text-sm">
+          Diseño inspirado en el proyecto de <a
+            className="text-main"
+            href="https://moure.dev"
+            target="_blank"
+            rel="noreferrer"
+          >
+            @mouredev
+          </a>
+          .
+        </p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
