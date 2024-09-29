0e273a2ee2c490b8f65e17dcb5e740cb8e018592
jesusmarzor <jesusmarzor28@gmail.com>
2024-09-29 18:46:47 +0200

    Add: analytics and speed insights vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import "@/app/globals.css";
 import Header from "@/app/components/Header";
 import { PostsProvider } from "@/app/contexts/PostsContext";
 import Footer from "@/app/components/Footer";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 export default function RootLayout({
   children,
@@ -12,6 +14,8 @@ export default function RootLayout({
     <html lang="en">
       <body className="font-myFont text-white-default bg-black-default font-light min-h-screen">
         <main className="flex flex-col justify-between items-center min-h-screen">
+            <SpeedInsights/>
+            <Analytics/>
             <Header/>
             <PostsProvider>
               <div className="py-8 px-6 mb-auto w-full">
