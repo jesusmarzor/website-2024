95e1fb84d390a02cb0099c1655219ea15c8b3678
jesusmarzor <jesusmarzor28@gmail.com>
2024-10-05 18:37:59 +0200

    Add: translations

--- app/components/Experience.tsx
+++ app/components/Experience.tsx
@@ -1,53 +1,55 @@
 import { SectionLayout } from "@/app/layouts/Section"
 import { TimeLineLayout } from "@/app/layouts/TimeLine"
+import { useTranslations } from "next-intl"
 
 const Experience = () => {
+    const t = useTranslations()
     return (
         <SectionLayout
-          title="Experiencia"
+          title={t("common.experience")}
         >
           <TimeLineLayout
-            title="iOS app developer"
-            subtitle="Sept 2022 - Actualidad | Deloitte, Madrid"
+            title={t("home.experience.secondJob.name")}
+            subtitle={t("home.experience.secondJob.time")}
           >
             <section className="flex flex-col justify-center items-start gap-1 ml-4">
               <h2 className="font-bold text-main mt-3">
-                Mid software developer
+                {t("home.experience.secondJob.secondCategory.name")}
               </h2>
               <p className="font-semibold text-sm">
-                Sept 2024 - Actualidad
+                {t("home.experience.secondJob.secondCategory.time")}
               </p>
             </section>
             <section className="flex flex-col justify-center items-start gap-1 ml-4">
               <h2 className="font-bold text-main mt-[.8rem]">
-                Junior software developer
+                {t("home.experience.secondJob.firstCategory.name")}
               </h2>
               <p className="font-semibold text-sm">
-                Sept 2022 - Ag 2024
+                {t("home.experience.secondJob.firstCategory.time")}
               </p>
               <ul className="ml-8 mt-2">
-                <li className="list-disc">Desarrollo en UIKit y Swift UI con arquitectura VIPER.</li>
-                <li className="list-disc">Test unitarios con XCTest.</li>
-                <li className="list-disc">Estimación y reparto de tareas con metodología SCRUM.</li>
-                <li className="list-disc">Mantenimiento de aplicaciones en producción.</li>
+                <li className="list-disc">{t("home.experience.secondJob.firstCategory.tasks.task1")}</li>
+                <li className="list-disc">{t("home.experience.secondJob.firstCategory.tasks.task2")}</li>
+                <li className="list-disc">{t("home.experience.secondJob.firstCategory.tasks.task3")}</li>
+                <li className="list-disc">{t("home.experience.secondJob.firstCategory.tasks.task4")}</li>
               </ul>
             </section>
           </TimeLineLayout>
           <TimeLineLayout
-            title="Fontend web developer"
-            subtitle="Feb 2022 - Ag 2022 | Secrets-On, Madrid"
+            title={t("home.experience.firstJob.name")}
+            subtitle={t("home.experience.firstJob.time")}
           >
             <ul className="ml-8 mt-2">
-              <li className="list-disc">Desarrollo de una plataforma online con React.</li>
+              <li className="list-disc">{t("home.experience.firstJob.tasks.task1")}</li>
               <li className="list-disc">
-                Recomendaciones que aporté:
+                {t("home.experience.firstJob.tasks.task2.name")}
                 <ul className="ml-8">
-                  <li className="list-disc">Trabajar con una metodología ágil.</li>
-                  <li className="list-disc">Usar SASS junto a Suit CSS.</li>
-                  <li className="list-disc">Una estructura de carpetas más sostenible.</li>
+                  <li className="list-disc">{t("home.experience.firstJob.tasks.task2.subtask1")}</li>
+                  <li className="list-disc">{t("home.experience.firstJob.tasks.task2.subtask2")}</li>
+                  <li className="list-disc">{t("home.experience.firstJob.tasks.task2.subtask3")}</li>
                 </ul>
               </li>
-              <li className="list-disc">Desarrollo de una página web en Wordpress.</li>
+              <li className="list-disc">{t("home.experience.firstJob.tasks.task3")}</li>
             </ul>
           </TimeLineLayout>
         </SectionLayout>
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,25 +4,31 @@ import { PostsProvider } from "@/app/contexts/PostsContext";
 import Footer from "@/app/components/Footer";
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from "@vercel/analytics/react"
+import {NextIntlClientProvider} from 'next-intl';
+import { getLocale, getMessages } from 'next-intl/server';
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const locale = await getLocale();
+  const messages = await getMessages();
   return (
-    <html lang="en">
+    <html lang={locale}>
       <body className="font-myFont text-white-default bg-black-default font-light min-h-screen">
         <main className="flex flex-col justify-between items-center min-h-screen">
             <SpeedInsights/>
             <Analytics/>
-            <Header/>
-            <PostsProvider>
-              <div className="py-8 px-6 mb-auto w-full">
-                {children}
-              </div>
-            </PostsProvider>
-            <Footer/>
+            <NextIntlClientProvider messages={messages}>
+              <Header/>
+              <PostsProvider>
+                <div className="py-8 px-6 mb-auto w-full">
+                  {children}
+                </div>
+              </PostsProvider>
+              <Footer/>
+            </NextIntlClientProvider>
         </main>
       </body>
     </html>
