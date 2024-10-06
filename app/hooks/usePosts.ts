f949cfbaff1bfa3f85d46116363917eabb196da8
jesusmarzor <jesusmarzor28@gmail.com>
2024-10-06 11:44:42 +0200

    Fix: get posts with validated date

--- app/hooks/usePosts.ts
+++ app/hooks/usePosts.ts
@@ -13,6 +13,7 @@ const usePosts = () => {
         .from('Posts')
         .select()
         .eq("lang", locale)
+        .lte("created_at", new Date().toISOString())
         .order("date", {ascending: false})
         .then( res => {
             const posts = res.data?.filter( ({date}) => isShowedPost(date)) as Post[]
